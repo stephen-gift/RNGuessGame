bc54283b359ead7fba6db44ac2cf3aa66ab23e4c
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-17 13:49:42 +0100

    screen and component setup

---
+++ screens/GameScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function GameScreen() {
+  return (
+    <View>
+      <Text>GameScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
