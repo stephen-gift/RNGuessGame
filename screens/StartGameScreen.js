13a4fb13343431ced761b0e5a60748dc8ed870d0
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-17 14:21:56 +0100

    Android and IOS styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -11,4 +11,18 @@ export default function StartGameScreen() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4, // ANDROID SHADOW
+    //IOS SHADOW
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
